41e910ee224bc3daf665454b452e4c7544c1b52c
auddrriiicc <062340722950@student.polsri.ac.id>
2026-09-25 11:01:45 +0700

    Add Vercel

--- server.js
+++ server.js
@@ -62,8 +62,11 @@ app.post('/api/masyarakat/register', async (req, res) => {
   }
 });
 
-// Port dinamis untuk cloud server
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server API berjalan di port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
+
+// WAJIB Ditambahkan untuk Vercel Serverless:
+module.exports = app;
